212e9aaf5ebc3a51df42e4c08724ed9b1fbe6c77
Kelly Sheddy <jungaka07@gmail.com>
2018-11-08 13:16:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,14 @@
 def display_board(board)
-  board = (" "," "," "," "," "," "," "," "," "," ")
+  
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+board = (" ", " ", " ", " ", " ", " ", " ", " ", " ", " ")
+
 
 def valid_move?(board, index)
   if index.between?(0, 8) == true && position_taken?(board, index) == false
